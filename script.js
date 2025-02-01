ea8e778a9da45d2d40a94141e65fcb3307ec79f9
Виктория <victoriagalchenko@gmail.com>
2025-02-01 14:18:09 +0100

    Добавление логотип, кнопок

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Portfolio</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <header>
-    <h1>Welcome to My Portfolio!</h1>
-  </header>
+// script.js
+const themeToggleButton = document.querySelector('.theme-toggle');
+const body = document.body;
 
-  <section id="about">
-    <h2>About Me</h2>
-    <p>Привет. Меня зовут Виктория. Проектирую интерфейсы, провожу исследования, выявляю проблемы и формулирую гипотезы. Cоздаю прототипы, тестирую их с пользователями и собираю обратную связь. Передаю макеты в разработку и контролирую качество вёрстки. Пишу код и смотрю в аналитику. Посетил 6 стран и 20 городов. По вечерам играю в PlayStation.
-    </p>
-  </section>
-
-  <section id="projects">
-    <h2>Мои работы</h2>
-    <ul>
-      <li><a href="#">LinkedIn Redesign</a></li>
-      <li><a href="#">Colory App UI</a></li>
-      <li><a href="#">Приложение для поиска и продажи билетов</a></li>
-    </ul>
-  </section>
-
-  <footer>
-    <p>Contact me at <a href="mailto:your-email@example.com">your-email@example.com</a></p>
-  </footer>
-
-  <script src="script.js"></script>
-</body>
-</html>
+themeToggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
